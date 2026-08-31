d7e1dd83df760d57d6e0103eeaa6d1fd2ceebaa4
mzaidiii <mohdmurtaza153@gmail.com>
2026-08-31 12:19:02 +0530

    JWT added

--- src/main/java/org/personal/ctmss/config/SecurityConfig.java
+++ src/main/java/org/personal/ctmss/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,7 +29,8 @@ public class SecurityConfig {
                         .requestMatchers("/actuator/**", "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
-                                "/v3/api-docs.yaml").permitAll()
+                                "/v3/api-docs.yaml",
+                                "/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/trails/**", "/api/trials/**").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/trails/**", "/api/trials/**").hasAnyRole("ADMIN", "PI")
                         .requestMatchers(HttpMethod.PATCH, "/api/trails/**", "/api/trials/**").hasAnyRole("ADMIN", "PI")
@@ -57,5 +60,10 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
