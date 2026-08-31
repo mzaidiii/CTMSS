70686de91a2926c1e878950386fea0078da9c14c
mzaidiii <mohdmurtaza153@gmail.com>
2026-08-31 12:20:56 +0530

    DTO for auth

---
+++ src/main/java/org/personal/ctmss/dtos/AuthRequest.java
@@ -0,0 +1,3 @@
+package org.personal.ctmss.dtos;
+
+public record AuthRequest(String username, String password) {}
---
+++ src/main/java/org/personal/ctmss/dtos/AuthResponse.java
@@ -0,0 +1,3 @@
+package org.personal.ctmss.dtos;
+
+public record AuthResponse(String token, String username, String role) {}
